bd141e66378caec74e9c9a21644c2e7f419b3abc
AnhQuangCee <duonganhquang97@gmail.com>
2018-12-17 15:59:03 +0700

    Create router, fix some error css, import picture

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,25 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 /* GET cropSelection page. */
 router.get('/CropSelection', function(req, res, next) {
-  res.render('cropSelection', { title: 'Express' });
+  res.render('cropSelection', { title: 'Crop Selection' });
+});
+
+/* GET Automation page. */
+router.get('/automation', function(req, res, next) {
+  res.render('automation', { title: 'Automation' });
+});
+
+/* GET ControlByHand page. */
+router.get('/controlByHand', function(req, res, next) {
+  res.render('controlByHand', { title: 'Control By Hand' });
+});
+
+/* GET ControlByHand page. */
+router.get('/404_PageNotFound', function(req, res, next) {
+  res.render('404_PageNotFound', { title: '404 Page Not Found' });
 });
 
 module.exports = router;
